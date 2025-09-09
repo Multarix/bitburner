4581b831b62c376b45e63e2c508f01cdbdcfcb51
Multarix <darien2105@gmail.com>
2025-09-10 00:07:40 +1000

    fixes and stuff

--- managers/autoBuyer.js
+++ managers/autoBuyer.js
@@ -16,7 +16,7 @@ export async function main(ns){
 			await ns.sleep(10000);
 		}
 
-		const owns = {
+		const programs = {
 			"BruteSSH.exe": ns.fileExists("BruteSSH.exe"),
 			"FTPCrack.exe": ns.fileExists("FTPCrack.exe"),
 			"RelaySMTP.exe": ns.fileExists("RelaySMTP.exe"),
@@ -30,26 +30,25 @@ export async function main(ns){
 		while((!RAMFine || !coresFine || (boughtItems < 5))){
 			const server = ns.getServer("home");
 			const money = server.moneyAvailable;
-			const currentRam = server.maxRam;
-			const currentCores = server.cpuCores;
+			// const currentRam = server.maxRam;
+			// const currentCores = server.cpuCores;
 
 			RAMFine = true;
 			coresFine = true;
 
 			boughtItems = 0;
-			for(const item in owns){
-				if(owns[item]){
+			for(const item in programs){
+				if(programs[item]){
 					boughtItems += 1;
 					continue;
 				}
 
 				ns.print(boughtItems);
-				if(boughtItems === 5) break;
 
 				const cost = ns.singularity.getDarkwebProgramCost(item);
 				if(money > cost * 1.2){
 					const boughtItem = ns.singularity.purchaseProgram(item);
-					owns[item] = boughtItem;
+					programs[item] = boughtItem;
 					if(boughtItem){
 						ns.toast(`Bought '${item}' for $${ns.formatNumber(cost, 3, 1000, true)}`, "info", 10000);
 						ns.print(`Bought ${Color.set(item, Color.preset.yellow)} for ${Color.set("$" + ns.formatNumber(cost, 3, 1000, true)), Color.preset.lime}`);
@@ -57,8 +56,11 @@ export async function main(ns){
 				}
 			}
 
+			/*
 			if(currentRam < 64000){ // 64TB
 				RAMFine = false;
+
+				ns.singularity.travelToCity("Sector-12");
 				const ramCost = ns.singularity.getUpgradeHomeRamCost();
 				if(money > ramCost * 1.2){
 					ns.singularity.upgradeHomeRam();
@@ -70,6 +72,7 @@ export async function main(ns){
 
 			if(currentCores < 8){
 				coresFine = false;
+
 				const coreCost = ns.singularity.getUpgradeHomeCoresCost();
 				if(money > coreCost * 1.2){
 					ns.singularity.upgradeHomeCores();
@@ -78,7 +81,7 @@ export async function main(ns){
 				}
 
 			}
-
+			*/
 			await ns.sleep(1000);
 		}
 	} catch (e){
